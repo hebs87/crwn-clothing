d75a5460bd923e45accbc0e433daf4afb34fdc12
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-12-19 12:08:36 +0000

    Added custom styles to homepage menu items

--- crwn-clothing/src/components/menu-item/menu-item.component.jsx
+++ crwn-clothing/src/components/menu-item/menu-item.component.jsx
@@ -12,14 +12,13 @@ import './menu-item.styles.scss';
 // We also want to add the size to our className, if it
 // is present (in the raw json data)
 const MenuItem = ({ title, imageUrl, size }) => (
-    <div
-        style={{
-            backgroundImage: `url(${ imageUrl })`
-        }} 
-        className={`${size} menu-item`}
-    >
+    <div className={`${size} menu-item`}>
+        <div
+            className='background-image'
+            style={{ backgroundImage: `url(${ imageUrl })` }}>
+        </div>
         <div className='content'>
-            <h1 className='title'>{ title }</h1>
+            <h1 className='title'>{ title.toUpperCase() }</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
