87af9bfb6ff6b9203ed4c38f0b038f6f49c26d65
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-12-19 15:13:50 +0000

    Installed react-router-dom and imported BrowserRouter into the project

--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -2,5 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// Import BrowserRouter to allow routing functionality
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// We need to wrap our App Component with the BrowserRouter
+// This gives the App the routing functionality
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+);
