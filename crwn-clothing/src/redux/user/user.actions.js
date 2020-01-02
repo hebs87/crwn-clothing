b02ab9df4be81f31232692adaa49e4e894a47035
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-01-02 14:42:49 +0000

    Created action creator to be passed into userReducer

---
+++ crwn-clothing/src/redux/user/user.actions.js
@@ -0,0 +1,12 @@
+// setCurrentUser is our action which takes a user
+// parameter and is passed in to the reducer. The
+// user parameter is the value that we set our
+// currentUser to, which will be passed into the
+// payload property of the action.
+// This is used instead of saying setState()
+export const setCurrentUser = user => ({
+    // The type needs to be the same string that the
+    // reducer's switch statement is expecting
+    type: 'SET_CURRENT_USER',
+    payload: user
+});
