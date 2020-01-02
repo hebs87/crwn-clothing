e75540d1b7e9d25077dfad7b76fbdd28e8a1c4a1
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-01-02 21:32:21 +0000

    Implemented redirect functionality to take user to homepage if signed in

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import './App.css';
-// Import Header Component
-import Header from './components/header/header.component'
-// Import HomePage Component
-import HomePage from './pages/homepage/homepage.component';
-// Import SignInAndSignUp Component
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-// Import ShopPage Component
-import ShopPage from './pages/shop/shop.component'
 // Import Switch and Route to enable routing
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 // Import connect to enable the app to access redux
 import { connect } from 'react-redux';
-// Import setCurrentUser action to enable us to set currentUser
-import { setCurrentUser } from './redux/user/user.actions';
+
+import './App.css';
+
+// Import HomePage Component
+import HomePage from './pages/homepage/homepage.component';
+// Import ShopPage Component
+import ShopPage from './pages/shop/shop.component'
+// Import SignInAndSignUp Component
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+// Import Header Component
+import Header from './components/header/header.component'
 // Import auth from firebase utiils
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+// Import setCurrentUser action to enable us to set currentUser
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
     // We set the initial unsibscribeFromAuth value to null
@@ -63,12 +65,31 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path='/' component={ HomePage } />
                     <Route path='/shop' component={ ShopPage } />
-                    <Route path='/signin' component={ SignInAndSignUp } />
+                    <Route
+                        exact
+                        path='/signin'
+                        render={() => 
+                            this.props.currentUser ?
+                            (<Redirect to='/' />)
+                            :
+                            (<SignInAndSignUp />)
+                        }
+                    />
                 </Switch>
             </div>
         );
     }
 }
+
+// FOR REDIRECTING THE USER TO HOME PAGE IF SIGNED IN
+// We want to get our redux user state
+// We need to destructure our user reducer from the
+// state and return our currentUser prop, which is
+// equal to user.currentUser (from the user reducer)
+// This can then be passed in to our connect method
+const mapStateToProps = ({ user }) => ({
+    currentUser: user.currentUser
+})
 
 // The mapDispatchToProps function takes the dispatch property
 // and returns an object where the prop name will be whatever
@@ -85,4 +106,6 @@ const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+    mapStateToProps, mapDispatchToProps
+)(App);
