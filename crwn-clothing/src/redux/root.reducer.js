7676dc26920627e162f839489776443183dee10e
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-01-02 14:18:11 +0000

    Created root reducer and imported userReducer into it

--- crwn-clothing/src/redux/root.reducer.js
+++ crwn-clothing/src/redux/root.reducer.js
@@ -1,3 +1,9 @@
 // combineReducers component allows us to
 // combine all sub-reducers into one master
 import { combineReducers } from 'redux';
+// Import userReducer
+import userReducer from './user/user.reducer';
+
+export default combineReducers({
+    user: userReducer
+})
