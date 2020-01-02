e3d8ac28884a41bb8965281c64370919f9c06e0f
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-01-02 14:04:38 +0000

    Created user reducer to enable redux state change for currentUser

---
+++ crwn-clothing/src/redux/user/user.reducer.js
@@ -0,0 +1,33 @@
+// We want to set our default state here, which
+// will be passed into the reducer as the default
+// This was what we initially set in the App.js
+// file, but we're just moving it into here instead
+const INITIAL_STATE = {
+    currentUser: null
+}
+
+// We set INITIAL_STATE as the default value of state
+const userReducer = (state = INITIAL_STATE, action) => {
+    // Use a switch statement to check the action.type
+    switch(action.type) {
+        // If the type is SET_CURRENT_USER
+        case 'SET_CURRENT_USER':
+            // We return a new object where we spread in
+            // the state, and we update the
+            // currentUser property valie to the payload
+            // React components ONLY re-render IF the props
+            // are different, so we need to use an object to
+            // update the props, so the object it has is new
+            return {
+                ...state,
+                currentUser: action.payload
+            }
+
+        // The default value will be our previous state
+        // if the type doesn't match the reducer's congfig
+        default:
+            state;
+    }
+};
+
+export default userReducer;
