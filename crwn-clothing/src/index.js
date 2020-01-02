e0e093bd2789b9e9e3bfa42784b003fa60c18957
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-01-02 14:36:09 +0000

    Imported store into index.js and passed into Provider component

--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -6,11 +6,13 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 // Import Provider to give our app access to Redux store and reducers
 import { Provider } from 'react-redux';
+// Import store so we can pass it in to our Provider
+import store from './redux/store';
 
 // We need to wrap our App Component with the BrowserRouter
 // This gives the App the routing functionality
 ReactDOM.render(
-    <Provider>
+    <Provider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
