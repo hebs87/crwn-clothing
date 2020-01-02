4c488216e3927ef11e94cb0ad5cc26e82b404822
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-01-02 13:01:52 +0000

    Imported Provider component into index.js to enable redux setup

--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // Import BrowserRouter to allow routing functionality
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+// Import Provider to give our app access to Redux store and reducers
+import { Provider } from 'react-redux';
 
 // We need to wrap our App Component with the BrowserRouter
 // This gives the App the routing functionality
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 );
