eefef53d25b30a52792928ea40fa85b471a6f59e
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-01-05 19:10:15 +0000

    Imported selectors into CheckoutPage component, created basic header and total price section in it

--- crwn-clothing/src/pages/checkout/checkout.component.jsx
+++ crwn-clothing/src/pages/checkout/checkout.component.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
+// Import connect to enable pulling state
+import { connect } from 'react-redux';
+// Import createStructuredSelector
+import { createStructuredSelector } from 'reselect';
+// Import selectCartItems and selectCartTotal selectors
+import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 // Import style sheet
 import './checkout.styles.scss';
 
-const CheckoutPage = () => (
-    <div>
-        CHECKOUT PAGE
+const CheckoutPage = ({ cartItems, total }) => (
+    <div className='checkout-page'>
+        <div className='checkout-header'>
+            <div className='header-block'>
+                <span>Product</span>
+            </div>
+            <div className='header-block'>
+                <span>Description</span>
+            </div>
+            <div className='header-block'>
+                <span>Quantity</span>
+            </div>
+            <div className='header-block'>
+                <span>Price</span>
+            </div>
+            <div className='header-block'>
+                <span>Remove</span>
+            </div>
+        </div>
+        {
+            // Map over our cartItems
+            cartItems.map(cartItem =>
+                cartItem.name
+            )
+        }
+        <div className='total'>TOTAL: &pound;{total}</div>
     </div>
 );
 
-export default CheckoutPage;
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems,
+    total: selectCartTotal
+});
+
+export default connect(mapStateToProps)(CheckoutPage);
