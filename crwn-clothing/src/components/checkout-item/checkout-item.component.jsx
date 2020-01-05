b83d7a3a519d5024f7c737a5ecb343d3933d7070
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-01-05 21:44:34 +0000

    Implemented functionality to remove checkout item in checkout page using actions and reducers

--- crwn-clothing/src/components/checkout-item/checkout-item.component.jsx
+++ crwn-clothing/src/components/checkout-item/checkout-item.component.jsx
@@ -1,22 +1,48 @@
 import React from 'react';
+// Import connect to bind action to component
+import { connect } from 'react-redux';
+// Import clearItemFromCart action to allow removing item from cart
+import { clearItemFromCart } from '../../redux/cart/cart.actions';
 // Import style sheet
 import './checkout-item.styles.scss';
 
 // We pass in the cartItem prop from the CheckoutPage component
-// and destructure the props that we need from it, which we
-// pass in to the relevant sections of our component
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
-    <div className='checkout-item'>
-        <div className='image-container'>
-            <img src={ imageUrl } alt={ name } />
+// We need to do the explicit return in the function
+// and then destructure our props in the function so that
+// we still have access to the whole cartItem, which will
+// let us pass it into the remove button to clear the item
+// We also pass in our clearItem function which we created
+// in the mapDispatchToProps function
+const CheckoutItem = ({ cartItem, clearItem }) => {
+    // Destructure the relevant props from the cartItem
+    // so we can pass it into the relevant sections
+    const { name, imageUrl, price, quantity } = cartItem;
+    return (
+        <div className='checkout-item'>
+            <div className='image-container'>
+                <img src={ imageUrl } alt={ name } />
+            </div>
+            <span className='name'>{ name }</span>
+            <span className='quantity'>{ quantity }</span>
+            <span className='price'>{ price }</span>
+            <div
+                className='remove-button'
+                onClick={() => clearItem(cartItem)}
+            >
+                &#10005;
+            </div>
         </div>
-        <span className='name'>{ name }</span>
-        <span className='quantity'>{ quantity }</span>
-        <span className='price'>{ price }</span>
-        <div className='remove-button'>
-            &#10005;
-        </div>
-    </div>
-);
+    )
+};
 
-export default CheckoutItem;
+// This allows us to pass in the item to the
+// clearItemFromCart action and dispatch the action
+// to our reducer - to clear an item from the cart
+const mapDispatchToProps = dispatch => ({
+    clearItem: item => dispatch(clearItemFromCart(item))
+});
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(CheckoutItem);
