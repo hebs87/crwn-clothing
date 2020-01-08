fbb04c6f2b020c2b370f653b9837d5c15b31a130
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-01-08 14:02:34 +0000

    Normalized shot data and selector to optimize query search

--- crwn-clothing/src/redux/shop/shop.selectors.js
+++ crwn-clothing/src/redux/shop/shop.selectors.js
@@ -1,17 +1,5 @@
 import { createSelector } from 'reselect';
 
-// To enable us to render the correct collection, depending on
-// which one the user navigates to, we need to create an object
-// that maps the string value (hats, jackets, etc.) to the
-// relevant ID, as the ID is a number value
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-};
-
 // Create input selector to get the shop reducer part of the rootReducer
 const selectShop = state => state.shop;
 
@@ -26,13 +14,12 @@ export const selectShopCollections = createSelector(
 // We use 'currying' to create a function that takes the
 // collectionUrlParam (hats/jackets, etc.) and then uses the
 // createSelector method to take the collections from the
-// selectShopCollections selector, and then use find() to
-// get the collection.id matching the URL parameter of
-// the COLLECTION_ID_MAP
+// selectShopCollections selector. It finds the collections
+// at the collectionUrlParam (the string that is passed in
+// from the mapStateToProps function in the Collection file)
 export const selectShopCollection = collectionUrlParam =>
     createSelector(
         [selectShopCollections],
-        collections => collections.find(
-            collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-        )
+        collections => 
+            collections[collectionUrlParam]
     );
