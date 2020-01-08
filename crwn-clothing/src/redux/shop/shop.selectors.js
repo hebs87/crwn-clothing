30384fe10d9d3219f62e6fe010fcd01c00d2e788
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-01-08 08:33:28 +0000

    Created ID map and selector to find collection ID using the collection URL param

--- crwn-clothing/src/redux/shop/shop.selectors.js
+++ crwn-clothing/src/redux/shop/shop.selectors.js
@@ -1,5 +1,17 @@
 import { createSelector } from 'reselect';
 
+// To enable us to render the correct collection, depending on
+// which one the user navigates to, we need to create an object
+// that maps the string value (hats, jackets, etc.) to the
+// relevant ID, as the ID is a number value
+const COLLECTION_ID_MAP = {
+    hats: 1,
+    sneakers: 2,
+    jackets: 3,
+    womens: 4,
+    mens: 5
+};
+
 // Create input selector to get the shop reducer part of the rootReducer
 const selectShop = state => state.shop;
 
@@ -9,3 +21,18 @@ export const selectShopCollections = createSelector(
     [selectShop],
     shop => shop.collections
 );
+
+// This selector will use the selectShopCollections selector
+// We use 'currying' to create a function that takes the
+// collectionUrlParam (hats/jackets, etc.) and then uses the
+// createSelector method to take the collections from the
+// selectShopCollections selector, and then use find() to
+// get the collection.id matching the URL parameter of
+// the COLLECTION_ID_MAP
+export const selectShopCollection = collectionUrlParam =>
+    createSelector(
+        [selectShopCollections],
+        collections => collections.find(
+            collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+        )
+    );
