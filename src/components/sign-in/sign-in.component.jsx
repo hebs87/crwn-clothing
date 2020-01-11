e91eac5d332ab1b9d3823fdf262c733ec33882a3
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-01-11 23:59:50 +0000

    Imported CSS in JS in SignIn

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,8 +5,13 @@ import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component'
 // Import signInWithGoogle
 import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
-// Import style sheet
-import './sign-in.styles.scss';
+// Import styled components
+import {
+    SignInContainer,
+    SignInTitle,
+    ButtonsBarContainer
+} from './sign-in.styles';
+
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -54,8 +59,8 @@ class SignIn extends React.Component {
 
     render() {
         return(
-            <div className='sign-in'>
-                <h2 className='title'>I already have an account</h2>
+            <SignInContainer>
+                <SignInTitle>I already have an account</SignInTitle>
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
@@ -76,16 +81,16 @@ class SignIn extends React.Component {
                         required
                     />
 
-                    <div className='buttons'>
+                    <ButtonsBarContainer>
                         <CustomButton type='submit'>Sign In</CustomButton>
                         <CustomButton onClick={ signInWithGoogle } isGoogleSignIn >
                             Sign In With Google
                         </CustomButton>
-                    </div>
-                </form>                
-            </div>
-        )
+                    </ButtonsBarContainer>
+                </form>
+            </SignInContainer>
+        );
     }
-}
+};
 
 export default SignIn;
