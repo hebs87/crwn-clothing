4e28f10a6cd26a54674c0d6a559407b838c200a6
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-01-12 00:03:19 +0000

    Imported CSS in JS in SignUp

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -5,8 +5,11 @@ import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 // Import auth and createUserProfileDocument for signing up
 import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
-// Import style sheet
-import './sign-up.styles.scss';
+// Import styled components
+import {
+    SignUpContainer,
+    SignUpTitle
+} from './sign-up.styles';
 
 // We want to set a state so we create a Class component
 class SignUp extends React.Component {
@@ -67,8 +70,8 @@ class SignUp extends React.Component {
         const { displayName, email, password, confirmPassword } = this.state;
 
         return(
-            <div className='sign-up'>
-                <h2 className='title'>I do not have an account</h2>
+            <SignUpContainer>
+                <SignUpTitle>I do not have an account</SignUpTitle>
                 <span>Sign up with your email and password</span>
 
                 <form className='sign-up-form' onSubmit={this.handleSubmit}>
@@ -107,7 +110,7 @@ class SignUp extends React.Component {
 
                     <CustomButton type='submit'>SIGN UP</CustomButton>
                 </form>
-            </div>
+            </SignUpContainer>
         );
     }
 }
