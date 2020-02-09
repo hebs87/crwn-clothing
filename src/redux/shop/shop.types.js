9b39cdf8010935c69866f8105e7b779550622725
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-02-09 11:29:01 +0000

    updated shop types with new types for each stage of asynchronous fetching events

--- src/redux/shop/shop.types.js
+++ src/redux/shop/shop.types.js
@@ -1,5 +1,20 @@
 const ShopActionTypes = {
-    UPDATE_COLLECTIONS: 'UPDATE_COLLECTIONS'
+    // Initial type that we created
+    // UPDATE_COLLECTIONS: 'UPDATE_COLLECTIONS'
+    // Once we are at the stage of wanting to move
+    // our asynchronous get call from our Shop
+    // component into our reducer instead, we need
+    // to create new types for each stage of the
+    // asynchronous process
+    // This call tells Redux we are starting to
+    // fetch the data - before any data is fetched
+    FETCH_COLLECTIONS_START: 'FETCH_COLLECTIONS_START',
+    // This call is for when our data call comes back
+    // succefully, hopefully with the data
+    FETCH_COLLECTIONS_SUCCESS: 'FETCH_COLLECTIONS_SUCCESS',
+    // This call is for when our data call fails - if
+    // data connection is poor, connection is down, etc.
+    FETCH_COLLECTIONS_FAILURE: 'FETCH_COLLECTIONS_FAILURE'
 };
 
 export default ShopActionTypes;
