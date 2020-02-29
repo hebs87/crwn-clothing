6632b0508763dc8f907146cdb1bf7b8b54406b8b
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-02-29 20:50:11 +0000

    Converted ShopPage to functional component using Hooks to replace componentDidMount

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // Import Route to enable advanced routing
 import { Route } from 'react-router-dom';
 // Import connect to enable passing in mapDispatchToProps
@@ -13,9 +13,9 @@ import CollectionsOverviewContainer from '../../components/collections-overview/
 import CollectionPageContainer from '../collection/collection.container';
 
 
-// This needs to be a class based component, as we want
-// to pull the shop data from firebase and store it as
-// the component's state.
+// This was a class component, but we've now converted it
+// to a functional component, as we are using the useEffect()
+// Hoop to replace the componentDidMount() lifecycle method.
 // As the ShopPage is rendered with the route in App.js,
 // match is one of its props that ShopPage will have access
 // to. match holds the path that is being rendered, so if
@@ -28,34 +28,48 @@ import CollectionPageContainer from '../collection/collection.container';
 // and adds the string value of the relevant collectionId
 // to the url (hats, jackets, etc.) and then renders the
 // CollectionPage component
-class ShopPage extends React.Component {
-    componentDidMount() {
-        // We need to destructure fetchCollectionsStart
-        // from our props
-        const { fetchCollectionsStart } = this.props;
-        // We then call it the moment our component mounts,
-        // which then pulls in the data from the reducer
+// We need to destructure both match and fetchCollectionsStart
+// from our props
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+    // We use the useEffect Hook to behave like a
+    // componentDidMount() so that our fetchCollectionsStart
+    // function is called when the component first renders
+    useEffect(() => {
+        // We call fetchCollectionsStart the moment our
+        // component mounts, which then pulls in the
+        // data from the reducer
         fetchCollectionsStart();
-    };
-
-    render() {
-        // We destructure the match from our props
-        const { match } = this.props;
-
-        return (
-            <div className='shop-page'>
-                <Route
-                    exact
-                    path={ `${match.path}` }
-                    component={ CollectionsOverviewContainer }
-                />
-                <Route
-                    path={ `${match.path}/:collectionId` }
-                    component={ CollectionPageContainer }
-                />
-            </div>
-        )
-    }
+    // If we don't pass in the second argument, the Hook
+    // will get called twice. This is because it will be
+    // dependent on the parent component (App.js). When
+    // App.js renders, it checks the user session and
+    // then re-renders if a user is logged in, as the
+    // currentUser prop value changes (initially the
+    // value us null, but then it changes to the value
+    // of the user that is logged in). This means that,
+    // on the first render, the ShopPage component will
+    // also render, and subsequently its useEffect(),
+    // then when it re-renders, the cycle repeats itself.
+    // This causes a 'second flicker' in the running app.
+    // Passing in the second argument ensures we only
+    // listen for the fetchCollectionStart prop, so the
+    // useEffect() only re-renders if the value of this
+    // prop changes
+    }, [fetchCollectionsStart]);
+    
+    return (
+        <div className='shop-page'>
+            <Route
+                exact
+                path={`${match.path}`}
+                component={CollectionsOverviewContainer}
+            />
+            <Route
+                path={`${match.path}/:collectionId`}
+                component={CollectionPageContainer}
+            />
+        </div>
+    )
 };
 
 // The mapDispatchToProps uses a dispatch function which
