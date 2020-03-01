fe1e1c0b15d93ea400a2f63f1031ae18d010c1c9
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-03-01 23:17:31 +0000

    Installed axios and configured StripeCheckoutButton to send token details to backend

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 // Import StripeCheckout
 import StripeCheckout from 'react-stripe-checkout';
+// Import axios to enable token creation
+import axios from 'axios';
 
-// We need to create an onToken action here, but for us,
-// it's just to display an alert that the payment has
-// been successful. We would need to configure this further
-// if we want to handle the charge with our backend though
-const onToken = token => {
-    alert('Payment Successful');
-};
 
 // We create our component which renders the StripeCheckoutButton
 // which takes the price prop (this will be the total that is
@@ -20,6 +15,46 @@ const StripeCheckoutButton = ({ price }) => {
     // This is the publishable key in stripe. As it is stored
     // in the .env file, we have to use process.env to access it
     const publishableKey = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY;
+
+    // We need to create an onToken action here, which
+    // creates a token that will be passed to our backend
+    // server. It will hold all the details that the
+    // server needs to make a valid charge to Stripe
+    const onToken = token => {
+        // Axios is a function that receives an object
+        // that has all of the actual properties that we
+        // want to pass to our backend, in order for it
+        // to know what type of request we’re trying to make
+        axios({
+            // We specify the URL of payment, so it will
+            // append payment to the end of the URL we are
+            // currently at
+            url: 'payment',
+            // Specify the method, which is post
+            method: 'post',
+            // The data represents the actual data that we
+            // want to pass through to the backend
+            data: {
+                // The amount goes to the priceForStripe const
+                amount: priceForStripe,
+                // The token will be the token object
+                token
+            }
+        // Now we check what type of response we are getting -
+        // success or error
+        }).then(response => {
+            // If it is successful, we display a success alert
+            alert('Payment successful')
+        }).catch(error => {
+            // If it is an error, we console log the error
+            console.log('Payment error ' + JSON.parse(error));
+            // We also display an error alert for the user
+            alert(
+                'There was an issue with your payment. Please make sure you use the provided credit card.'
+            );
+        });
+    };
+
     // Now we return the stripe button and pass in the
     // properties that we need
     return (
