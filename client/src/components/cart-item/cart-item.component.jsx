52dea61542b38d134b8e09b792b5ee997c618b61
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-03-07 09:39:45 +0000

    Used React.memo to memoize CartItem component to prevent unnecessary re-rendering

--- client/src/components/cart-item/cart-item.component.jsx
+++ client/src/components/cart-item/cart-item.component.jsx
@@ -19,4 +19,13 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
     </CartItemContainer>
 );
 
-export default CartItem;
+// React.memo memoizes the component. Currently, when we add new
+// items to the cart, a new array of cartItems is being passed in
+// to the CartDropdown each time, even if a different item is added
+// (e.g. 5 items are added – 3 x jeans and 2 x shirts. The first
+// jeans item renders all 5 times, instead of 3 times when it count
+// increases). We want our cart item to only re-render if its value
+// changes (e.g. Jeans only re-renders when it is added or if its
+// quantity changes. Memoizing the CartItem component, means that
+// the component is only re-rendered when its value changes
+export default React.memo(CartItem);
